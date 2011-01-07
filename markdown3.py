b2be37be8e5c5ea611041c93e6fba651fce8399a
Ed Singleton <singletoned@gmail.com>
2011-01-07 12:13:24 +0000

    Added support for italics

--- markdown3.py
+++ markdown3.py
@@ -5,5 +5,9 @@ import pyparsing as pp
 def parse(text):
     strong_start = pp.Literal("**").setParseAction(pp.replaceWith("<strong>"))
     strong_end = pp.Literal("**").setParseAction(pp.replaceWith("</strong>"))
-    sentence = strong_start + pp.SkipTo(strong_end) + strong_end
+    strong_phrase = strong_start + pp.SkipTo(strong_end) + strong_end
+    em_start = pp.Literal("*").setParseAction(pp.replaceWith("<em>"))
+    em_end = pp.Literal("*").setParseAction(pp.replaceWith("</em>"))
+    em_phrase = em_start + pp.SkipTo(em_end) + em_end
+    sentence = pp.MatchFirst([strong_phrase, em_phrase])
     return sentence.transformString(text)
--- test_markdown3.py
+++ test_markdown3.py
@@ -2,8 +2,11 @@
 
 import markdown3
 
-def test_bold():
+def test_emphasis():
     data = [
+        ("*Hello*", "<em>Hello</em>"),
+        ("*Hello World*", "<em>Hello World</em>"),
+        ("Hello *World*", "Hello <em>World</em>"),
         ("**Hello**", "<strong>Hello</strong>"),
         ("**Hello World**", "<strong>Hello World</strong>"),
         ("Hello **World**", "Hello <strong>World</strong>"),
