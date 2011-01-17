1d484b837b039cd27d2312c6f03f83ecc374ac0d
Ed Singleton <singletoned@gmail.com>
2011-01-17 13:27:29 +0000

    Added another test for emphasis

--- test_markdown3.py
+++ test_markdown3.py
@@ -13,3 +13,12 @@ def test_emphasis():
     expected = ['body', [['plain', "Hello "], ['emphasis', "World"]]]
     result = markdown3.parse(data)
     assert expected == result
+
+    data = "Text with *some bold* in it"
+    expected = [
+        'body',
+        [['plain', "Text with "],
+         ['emphasis', "some bold"],
+         ['plain', ' in it']]]
+    result = markdown3.parse(data)
+    assert expected == result
